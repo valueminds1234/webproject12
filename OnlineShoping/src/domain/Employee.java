024cdd8d230f75d70f0696dd05b0f962e2c0e2b9
KRISHNA <maddelakrishnakumar@gmail.com>
2017-08-19 21:17:48 +0530

    employee bean and hibernate dependencies are added

--- OnlineShoping/src/domain/Employee.java
+++ OnlineShoping/src/domain/Employee.java
@@ -1,5 +1,48 @@
 package domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="emp011")
 public class Employee {
+	
+	@Id 
+	@GeneratedValue
+	private String eid;
+
+	@Column
+	private String ename;
+	private String email,mobile;
+	public String getEid() {
+		return eid;
+	}
+	public void setEid(String eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	
+	
 
 }
